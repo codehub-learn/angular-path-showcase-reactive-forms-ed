f8e44e1886db5b290275a7da05e66b77711070ce
Ioannis <jondan97@gmail.com>
2023-10-25 09:52:57 +0300

    initial form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-path-showcase-reactive-forms-ed';
+  form!: FormGroup;
+
+  constructor() {
+  }
+
+  private setFormInitialValues(){
+    this.form = new FormGroup<any>({
+      fullname: new FormControl()
+    });
+  }
 }
